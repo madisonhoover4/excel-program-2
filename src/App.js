31d3d02c57cadc0083867423ef36697458d92e86
madisonhoover <madisonhoover4@gmail.com>
2020-01-27 12:16:14 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Button from 'react';
+import Component from 'react';
+
+export default class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>"Button"</Button>
+      </header>
+    </div>
+  );
+  }
+
+}
